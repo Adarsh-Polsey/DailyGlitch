1d3e4c12a44eb4030ecff38a33d766788706a7e4
Adarsh-Polsey <adarshpolsey7@gmail.com>
2025-03-16 13:18:44 +0530

    new news

--- news/news_list.py
+++ news/news_list.py
@@ -1,33 +1,5 @@
-news_list=['''OpenAI and Elon Musk have agreed to fast-track a trial over OpenAI’s for-profit shift, the latest turn in a grudge match between the world’s richest person and OpenAI CEO Sam Altman playing out publicly in court.
-
-Billionaire Elon Musk and OpenAI jointly proposed a trial in December, according to a federal court filing on Friday.
-
-The parties agreed to delay a decision on whether the expedited case will be decided by a jury or solely by the judge, said the filing in U.S. District Court for the Northern District of California.
-
-The judge this month denied Musk’s request to pause the artificial intelligence group’s transition to a for-profit model but agreed to an expedited trial in the autumn, the latest turn in the high-stakes legal fight.
-
-“We welcome the court’s March 4 decision rejecting Elon Musk’s latest attempt to slow down OpenAI for his personal benefit,” OpenAI said in a blog post on Friday.
-
-Musk cofounded OpenAI with Altman in 2015 but left before the company took off and subsequently founded the competing startup xAI in 2023.''',
-'''A London court hearing, reported to be Apple’s appeal against a British government order to create a “back door” to its encrypted cloud storage systems, was held in secret on Friday, with media not allowed to attend despite a formal request.
-
-In February, The Washington Post reported that Britain had issued a “technical capability notice” to the tech firm to enable access to encrypted messages and photos, even for users outside the country.
-
-The iPhone maker in response removed its most advanced security encryption for cloud data, called Advanced Data Protection, for new users in Britain.
-
-Details of the case have been shrouded in secrecy, and neither Apple nor the British government has publicly confirmed the technical capability notice.
-
-The BBC reported a hearing on Friday simply listed as “an application in private” at the Investigatory Powers Tribunal, a court that considers allegations of unlawful intrusion by public bodies, was Apple’s appeal against this order.
-
-There was no confirmation of what parties were involved, although James Eadie, who represents the government in its most serious legal cases, attended. He declined to comment. Apple did not immediately respond to a request for comment.
-
-A lawyer representing 10 media organisations, including Reuters and the BBC, submitted an application to the tribunal for the case to be held in public.
-
-The court confirmed receipt of his email but he was not invited to appear before the judges to make any further submissions on Friday, and no reporters were allowed in the courtroom. The hearing concluded after about six hours.''',
-
-'''After spending nine months aboard the International Space Station (ISS), NASA astronauts Sunita Williams and Butch Wilmore are finally on track for their long-awaited return to Earth. A replacement crew launched on Friday evening, paving the way for the duo’s homecoming after what was initially expected to be a short-term mission.
-
-SpaceX’s Falcon 9 rocket took off from NASA’s Kennedy Space Center in Florida at 7:03 pm. ET, carrying four astronauts who will take over operations on the ISS. This new crew includes NASA astronauts Anne McClain and Nichole Ayers, both military pilots, as well as Japan’s Takuya Onishi and Russia’s Kirill Peskov, both former airline pilots. The incoming crew will spend the next six months at the space station, while Williams and Wilmore prepare for their return.
-
-The extended stay of Wilmore and Williams was marked by unexpected technical issues, political attention, and personal sacrifices. As test pilots for Boeing’s new Starliner capsule, the two astronauts had launched from Cape Canaveral on June 5, 2024, with the expectation of a brief stay. However, a series of technical setbacks — including helium leaks and thruster failures on the Starliner — forced NASA and Boeing into months of investigations, ultimately ruling the capsule unsafe for a return trip.'''
-]
+news_list=[
+    '''Microsoft is working on Copilot for Gaming, a new AI-powered assistant for Xbox gamers. The Redmond, Washington-based company said in a recent blog post that its upcoming assistant will save gamers' time by helping them find tutorials and other in-game assistance. Copilot for Gaming can also understand natural language commands to install (or update) games, while providing players with a recap of their progress. Microsoft says the feature will initially be available on smartphones, and Xbox Insiders will get early access to Copilot for Gaming.Copilot for Gaming to Adapt to Player Preferences for Tailored Experience.According to Xbox Corporate Vice President of Gaming AI, Fatima Kardar, Copilot for Gaming is an AI-powered gaming assistant for gamers. It can help gamers set up games on their Xbox, recommend new titles, help them with in-game assistance, or make social connections. It will offer gamers a tailored experience, based on their preferences and habits, Kardar said.''',
+    '''Google is reportedly working on adding support for a second language with Gemini Live. As per the report, the Mountain View-based tech giant is planning to allow users to speak with the artificial intelligence (AI) chatbot in two different languages across speech and text modalities. With this, Gemini Live is said to not only understand what the user is saying, but also be able to respond in the same language. The first evidence of this feature was reportedly found in December, and now the company has also updated its support pages to highlight the support for a second language. Gemini Live May Support a Second Language Right now, Gemini Live, the two-way real-time voice chat feature, allows users to switch between languages mid sentence, and the AI still understands and responds appropriately. With a bit of right prompting, users can also make Gemini speak in two languages at the same time, which is useful if the speaker prefers one of the hybrid language varieties such as Hinglish (blend of Hindi and English) or Spanglish (blend of Spanish and English). While this is a useful feature, sometimes Gemini Live will not recognise the second language despite multiple prompts. Additionally, support for a second language is not available outside of Gemini Live, which also restricts the usability.''',
+    '''Snapchat Gets AI Video Lenses In a newsroom post, the social media platform detailed the new AI feature. These AI Lenses are powered by the company's in-house generative video model. The company first unveiled these models at the Snap Partner Summit in September last year. At the time, the AI tools were available to a small group of creators in beta. The company is now expanding the technology to a larger user base. These new AI video Lenses will be available to Snapchat Platinum subscribers. Platinum is the most expensive subscription tier by the company with the price of Rs. 99 a month. The subscription offers an ad-free experience as well as other exclusive features.'''
+           ]
